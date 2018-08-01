67c5cbfad61da7d43a2f984b55071defefd1ce6e
Avanish Mishra <avanish.neham@gmail.com>
2018-08-01 13:16:38 -0400

    day 21( generics)

--- src/day21.java
+++ src/day21.java
@@ -1,4 +1,17 @@
 
 public class day21 {
 
+    /**
+    *    Method Name: printArray
+    *    Print each element of the generic array on a new line. Do not return anything.
+    *    @param A generic array
+    **/
+    
+    // Write your code here
+public static<E> void printArray(E[] inputArray){
+    for(E element: inputArray){
+        System.out.println(element);
+    }
+    
+}
 }
