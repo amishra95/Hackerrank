7b397f18fc5d594a42b0d43250720ecc0aec9109
Avanish Mishra <avanish.neham@gmail.com>
2018-08-01 13:30:53 -0400

    Triplets Hackerrank

--- src/TripletsQuestion.java
+++ src/TripletsQuestion.java
@@ -1,11 +1,33 @@
 /**
  * 
  */
+import java.util.Scanner;
+import java.util.HashSet;
 
 /**
  * @author Avanish Mishra
  *
  */
 public class TripletsQuestion {
+	public static void main(String[] args) {
+		
+		HashSet<Integer> set = new HashSet<>();
+		Scanner scan = new Scanner(System.in);
+		int size = scan.nextInt();
+		int d = scan.nextInt();
+		int count = 0;
+		
+	for(int i = 0; i < size; i++) {
+		int num = scan.nextInt();
+	if(set.contains(num - d) && set.contains(num - 2 *d)) {
+		
+		count++;
+		
+	}
+	set.add(num);
+	}
+	scan.close();
+	System.out.println(count);
+	}
 
 }
