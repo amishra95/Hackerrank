f5e2a013f87c753bb4384b5930f86d54b8d8d9ed
Avanish Mishra <avanish.neham@gmail.com>
2018-08-01 13:13:44 -0400

    getHeight(day22)

--- src/day22.java
+++ src/day22.java
@@ -1,4 +1,22 @@
+//Avanish Mishra
+// Hackerrank
 
 public class day22 {
-
+	public static int getHeight(Node root){
+		      if(root == null)
+	
+	          return -1;
+	
+	        int left = getHeight(root.left);
+	
+	        int right = getHeight(root.right);
+	
+	        
+	
+	        return Math.max(left, right) + 1;
+	
+	        
+	
+	    }
+	
 }
