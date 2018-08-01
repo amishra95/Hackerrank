4df7d2a61e6eddf8adb84bb6d91aa6dc0cec9e14
Avanish Mishra <avanish.neham@gmail.com>
2018-08-01 13:07:13 -0400

    New Addition to the protocol

--- src/day23.java
+++ src/day23.java
@@ -1,4 +1,27 @@
+import java.util.*;
+import java.io.*;
 
 public class day23 {
+	static void levelOrder(Node root){
+	      
+	    Queue<Node> queue = new LinkedList<>();
+	    
+	    if(root == null){
+	        return;
+	    }
+	    queue.add(root);
+	    while(!queue.isEmpty()){
+	        Node node = queue.remove();
+	        System.out.print(node.data + " ");
 
+	            if (node.left != null) {
+	                queue.add(node.left);
+	            }
+	            if (node.right != null) {
+	                queue.add(node.right);
+	            }
+
+	    }
+	    
+	    }
 }
