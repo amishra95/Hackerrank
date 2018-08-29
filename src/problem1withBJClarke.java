0e3d14a84bcfc4ae10e68e4af5c21aee7c7b01c7
Avanish Mishra <avanish.neham@gmail.com>
2018-08-28 22:52:24 -0400

    Solutions

---
+++ src/problem1withBJClarke.java
@@ -0,0 +1,23 @@
+
+public class problem1withBJClarke {
+	   static List<Integer> minimalOperations(List<String> words) {
+           // loop through each word
+         // loop through each char in each word
+             // if the current char equals the previous char, add 1 to the count and skip ahead an index (so you don't count the next one)
+     List<Integer> count = new ArrayList<Integer>();
+ 
+     for (int i = 0; i < words.size(); i++) {
+     int c = 0;
+     for (int j = 1; j < words.get(i).length(); j++) {
+       if (words.get(i).charAt(j) == words.get(i).charAt(j-1)) {
+             c++;
+             j++;
+       }
+     }
+     count.add(c);
+   }
+ 
+     return count;
+
+}
+}
---
+++ src/problem2WithBJClarke.java
@@ -0,0 +1,36 @@
+
+public class problem2WithBJClarke {
+
+    static List<Integer> closest(String s, List<Integer> queries) {
+        List<Integer> responses = new ArrayList<Integer>();
+  
+  for (int i = 0; i < queries.size(); i++) {
+    int query = queries.get(i);
+    char queryChar = s.charAt(query);
+    if (s.indexOf(queryChar) == s.lastIndexOf(queryChar)) {
+      responses.add(-1);
+      continue;
+    }
+    
+    // if true, there is an index > query such that s.charAt(index) == queryChar
+    if (query < s.length()-1 && s.indexOf(queryChar, query+1) != -1) {
+      
+      int index = s.indexOf(queryChar, query+1);
+      int diff = index - query;
+      
+      int leftIndex = s.lastIndexOf(queryChar, query-1);
+      if (leftIndex != -1 && query - leftIndex <= diff) {
+        responses.add(leftIndex);
+      } else {
+        responses.add(index);
+      }
+      continue;
+    }
+    responses.add(s.lastIndexOf(queryChar, query-1));
+  }
+  
+  return responses;
+
+}
+
+}
