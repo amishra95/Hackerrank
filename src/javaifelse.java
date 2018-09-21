76db9fe185829b7871f1aa5f1d971af36d708860
Avanish Mishra <avanish.neham@gmail.com>
2018-09-21 12:38:28 -0400

    javaIfElse statement

---
+++ src/javaifelse.java
@@ -0,0 +1,20 @@
+public class Solution {
+
+
+    public static void main(String[] args) {
+
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();            
+        String ans="";
+        if(n%2==1 || ( (n%2==0) && (n>=6 && n <= 20 ) ) ){
+          ans = "Weird";
+        }
+        else{
+            ans = "Not Weird";
+           //Complete the code
+
+        }
+        System.out.println(ans);
+
+    }
+}
