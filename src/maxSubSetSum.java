a44e6c2c148105b5aa29b4cd462c9855cbc66a6a
Avanish Mishra <avanish.neham@gmail.com>
2018-11-14 14:50:16 -0500

    maxSubSetSum
    two non-consequitive problems

---
+++ src/maxSubSetSum.java
@@ -0,0 +1,66 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    // Complete the maxSubsetSum function below.
+    static int maxSubsetSum(int[] arr) {
+     
+        int[] dp = new int[arr.length];
+        
+        if(arr.length == 0){
+            return 0;
+        }
+        if(arr.length == 1){
+            return arr[0];
+        }
+        
+    for(int i = 0; i < arr.length; i++){
+        if(i == 0){
+            dp[0] = Math.max(0, arr[0]);
+        }
+        else if( i == 1){
+            dp[1] = Math.max(arr[0], arr[1]);
+        }
+        else{
+            dp[i] = Math.max(arr[i] + dp[i-2], dp[i-1]);
+        }
+    }
+    
+
+    return dp[arr.length -1];
+        
+    }
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        int[] arr = new int[n];
+
+        String[] arrItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < n; i++) {
+            int arrItem = Integer.parseInt(arrItems[i]);
+            arr[i] = arrItem;
+        }
+
+        int res = maxSubsetSum(arr);
+
+        bufferedWriter.write(String.valueOf(res));
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
