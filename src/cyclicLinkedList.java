da68bdbd2c076d0953d85faff0a0ebb43098e91d
Avanish Mishra <avanish.neham@gmail.com>
2018-11-20 21:39:07 +0530

    hackerrank LinkedList ( easy Question)

---
+++ src/cyclicLinkedList.java
@@ -0,0 +1,18 @@
+
+public class cyclicLinkedList {
+	boolean hasCycle(Node head) {
+	    if (head == null) return false;
+	    
+	    Node slow = head;
+	    Node fast = head.next;
+	    while (slow != fast) {
+	        if (fast == null || fast.next == null) return false;
+	        
+	        slow = slow.next;
+	        fast = fast.next.next;
+	    }
+	    
+	    return true;
+	}
+
+}
