c52818e0278c46985123d2b8dab78af54108013f
Avanish Mishra <avanish.neham@gmail.com>
2019-09-26 16:51:52 +0530

    Update day22.java

--- src/day22.java
+++ src/day22.java
@@ -12,7 +12,7 @@ public class day22 {
 	        int right = getHeight(root.right);
 	
 	        
-	
+	// Find the max of either the left root side or the right side of the root and then go ahead.
 	        return Math.max(left, right) + 1;
 	
 	        
