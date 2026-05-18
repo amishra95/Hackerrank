5fadf6136d197a5089851ebb8844fefa721bc0c8
Avanish Mishra <avanish.neham@gmail.com>
2026-05-18 20:32:11 +0530

    Update isAnagram.java

--- src/isAnagram.java
+++ src/isAnagram.java
@@ -1,3 +1,29 @@
+//Mot efficient method, which also involved O(N) func.
+static boolean isAnagram(String a, String b) {
+        
+        java.util.HashMap<Character, Integer> map = new java.util.HashMap<>();
+        
+        char[] first = a.toLowerCase().toCharArray();
+        char[] second = b.toLowerCase().toCharArray();
+        
+        for(char c: first){
+            map.put(c, map.getOrDefault(c,0)+1);
+        }
+        
+        for(char c: second){
+            map.put(c, map.getOrDefault(c,0)-1);
+        }
+        
+        for(int count: map.values()){
+            if(count != 0){
+                return false;
+            }
+            
+        }
+        return true;
+
+    }
+
 //Solving without importing java.util.Arrays.*;
 public class Solution {
 
